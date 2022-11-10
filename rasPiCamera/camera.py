a1817251cf7b915e74dbbc28d05b3dfc6be265ab
Kelly Guan <kellyguan@v1040-wn-rt-b-26-245.campus-dynamic.uwaterloo.ca>
2022-11-10 17:04:58 -0500

    idk

--- rasPiCamera/camera.py
+++ rasPiCamera/camera.py
@@ -1,15 +1,12 @@
-
+from picamera import PiCamera
 import time 
 
-name = "NPC"
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.vflip = True
 
-camera = PiCamera() 
-camera.resolution = (640, 480) #resolution of camera 
-camera.framerate = 10
-camera.vflip = True #flips camera view to right 
+camera.start_preview()
+time.sleep(2)
 
-camera.start_preview() 
-time.sleep(2) #camera warming up 
-
-camera.start_recording("npc_show.h264") #name of recording 
-camera.stop_recording()  #stop recording 
+camera.start_recording("npc_show.h264")
+camera.stop_recording()

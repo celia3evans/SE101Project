4ef8d872647a1e3b67cdd4ec0f724c51125b1fb9
elaimizrahi <113472016+elaimizrahi@users.noreply.github.com>
2022-11-15 16:24:30 -0500

    Update driving.py

--- driving/driving.py
+++ driving/driving.py
@@ -1,1 +1,32 @@
-placeholder
+#Code to check if motors are connected to pi
+import Rpi.GPIO as gpio
+
+import time
+
+
+
+gpio.setmode(gpio.BOARD)
+
+gpio.setup(7, gpio.OUT)
+
+gpio.setup(11, gpio.OUT)
+
+gpio.setup(13, gpio.OUT)
+
+gpio.setup(15, gpio.OUT)
+
+
+
+gpio.output(7, True)
+
+gpio.output(11, True)
+
+gpio.output(13, False)
+
+gpio.output(15, False)
+
+time.sleep(0.5)
+
+gpio.cleanup( )
+
+#sourced from https://www.instructables.com/Raspberry-Pi-Robot-Car/
